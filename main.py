afee3e63a0493e4f1fc309cd16d1e5d7e3b6e061
kirienkovitaliy <kirienkovitaliy1989@gmail.com>
2023-07-10 16:08:23 +0300

    update tests

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import redis.asyncio as redis
 from fastapi import FastAPI
 from fastapi_limiter import FastAPILimiter
+
 from starlette.middleware.cors import CORSMiddleware
 
 from src.conf.config import settings
 from src.routes import contacts, auth, users
-
 
 app = FastAPI()
 
